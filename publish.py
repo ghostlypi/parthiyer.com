22b8b4f0e8f3a92eaeb440a29a2af8a633361cf9
Parth Iyer <parth.iyer@gmail.com>
2025-10-05 19:12:59 -0400

    "Published: 2025-10-05-19:12:59"

--- publish.py
+++ publish.py
@@ -9,7 +9,7 @@ header = '''<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog</title>
     <link rel="icon" href="images/favicon.png" type="image/png">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <nav>
