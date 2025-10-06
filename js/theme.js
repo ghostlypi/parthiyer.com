b0659706c1c26410da37b474e375254bd84ea21c
Parth Iyer <parth.iyer@gmail.com>
2025-10-05 21:02:59 -0400

    "Published: 2025-10-05-21:02:59"

--- js/theme.js
+++ js/theme.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
             date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
             expires = "; expires=" + date.toUTCString();
         }
-        document.cookie = name + "=" + (value || "") + expires + "; path=/; SameSite=Lax; Secure";
+        document.cookie = name + "=" + (value || "") + expires + "; path=/; SameSite=Strict; Secure";
     }
 
     function getCookie(name) {
